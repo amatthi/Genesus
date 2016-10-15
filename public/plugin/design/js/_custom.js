e5e5f981da8466735748784f75b1cca433e58201
amatthi <amatthi@emory.edu>
2016-10-14 23:44:23 -0700

    edits

--- public/plugin/design/js/_custom.js
+++ public/plugin/design/js/_custom.js
@@ -114,7 +114,7 @@ chisel_launch.controller('CustomCtrl', [
 
         /********** launch **************/
         $scope.launch_step = 'create';
-        $scope.campaign_data = { goal: 30, cost_per_bottle: 5.75, sale_price: 20.00 };
+        $scope.campaign_data = { goal: 30, cost_per_bottle: 5.75, sale_price: 24.99 };
         // $scope.amazon_connect('tappyn');
 
         $scope.launch_step_create = function() {
