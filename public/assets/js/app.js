57e9bd8000f8b11be966cefcdcbdf847d9086237
Onion Lian <onininon@dejavu.studio>
2016-10-19 14:18:12 +0800

    the other standard templates go away when either the pre-workout or whey-protein formula

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -10624,7 +10624,7 @@ chisel.controller("campaignController", function($scope, $rootScope,$routeParams
 
 chisel.controller("dashboardController", function($scope, $rootScope, $routeParams, mainFactory, chisel_var) {
     $scope.campaigns = [];
-
+    
     mainFactory.dashboard_campaigns().then(function(r){
     	$scope.campaigns = r.data;
     }, $scope.handle_error);
--- public/plugin/design/js/_custom.js
+++ public/plugin/design/js/_custom.js
@@ -206,7 +206,8 @@ chisel_launch.controller('CustomCtrl', [
         }
 
         $scope.hasFormula = function(product) {
-            if (product.name == 'Template' || product.name == 'Standard Bottle') {
+            var hide_standard = ['Pre-Workout (Fruit Punch)', 'Whey Protein Isolate (Vanilla)'];
+            if ((product.name == 'Template' || product.name == 'Standard Bottle') && $.inArray($scope.campaign_data.formula.name, hide_standard) == -1) {
                 return true;
             }
 
