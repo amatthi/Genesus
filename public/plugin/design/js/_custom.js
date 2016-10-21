8e7e8bc7e7cf5a090b0bc527c0f5743eb123233c
Onion Lian <onininon@dejavu.studio>
2016-10-21 12:20:59 +0800

    redirect the user to their dashboard after they launch

--- public/plugin/design/js/_custom.js
+++ public/plugin/design/js/_custom.js
@@ -164,7 +164,7 @@ chisel_launch.controller('CustomCtrl', [
                 window.onbeforeunload = null;
                 window.onhashchange = null;
                 alert('Your campaign has been saved!');
-                //window.location = '/dashboard';
+                window.location = '/dashboard';
             }, $scope.handle_error);
         }
 
