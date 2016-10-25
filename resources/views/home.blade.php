34359cb028b53676445088946ce9df93260bc2ec
amatthi <amatthi@emory.edu>
2016-10-25 09:22:49 -0700

    edits

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -28,6 +28,12 @@
 			<div class="modal_actions2" ng-if="now_module == 'payment'" ng-include="'/html/templates/payment.html?'+template_v" style="padding-left:100px;"></div>
 		</div>
 	<section class="innerpage" ng-view></section>
+	<script>
+  window.intercomSettings = {
+    app_id: "ifi52d9m"
+  };
+</script>
+<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ifi52d9m';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>
 </body>
 <script type="text/javascript" src="/assets/js/app.js"></script>
 <script type="text/javascript" src="https://js.stripe.com/v2/"></script>
