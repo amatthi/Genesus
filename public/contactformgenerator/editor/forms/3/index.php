66b5abc2a43a5811b927fc398e348f523a7738f9
amatthi <amatthi@emory.edu>
2016-11-08 15:06:21 -0800

    launch shut down

---
+++ public/contactformgenerator/editor/forms/3/index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1">
+
+<meta charset="utf-8">
+
+<title>I want to launch</title>
+
+
+<!-- Form Start -->
+
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+<script src="cfgen-form-3/js/form.js"></script>
+<link href="cfgen-form-3/css/form.css" rel="stylesheet" type="text/css">
+
+<!-- Form End -->
+
+
+</head>
+
+<body>
+
+<div class="cfgen-form-container" id="cfgen-form-3">
+
+<div class="cfgen-form-content">
+
+<div class="cfgen-e-c">
+
+	<div class="cfgen-e-set" id="cfgen-element-3-2-set-c">
+		<div class="cfgen-paragraph" id="cfgen-element-3-2-paragraph">
+		To contact us, use the form below.<br />
+We will get back to you as soon as possible.
+		</div>
+	</div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<label class="cfgen-label" id="cfgen-element-3-3-label" ><span class="cfgen-label-value">Email address</span><span class="cfgen-required">*</span></label>
+
+	<div class="cfgen-e-set" id="cfgen-element-3-3-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-3-3-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-3-3-input-c">
+				<input type="text" class="cfgen-type-email cfgen-form-value " name="cfgen-element-3-3" id="cfgen-element-3-3">
+			</div>
+		</div>
+	</div>
+
+	<div class="cfgen-clear"></div>
+
+</div>
+
+
+<div class="cfgen-e-c">
+
+	<div class="cfgen-e-set" id="cfgen-element-3-5-set-c">
+		<div class="cfgen-input-group" id="cfgen-element-3-5-inputgroup-c">
+			<div class="cfgen-input-c" id="cfgen-element-3-5-input-c">
+				<input type="submit" class="cfgen-submit" name="cfgen-element-3-5" id="cfgen-element-3-5" value="Join List">
+			</div>
+		</div>
+	</div>
+
+</div>
+
+
+<div class="cfgen-loading"></div>
+
+</div><!-- cfgen-form-content -->
+
+</div><!-- cfgen-form-container -->
+
+</body>
+
+
+</html>
