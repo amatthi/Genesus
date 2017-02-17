d450801fe38182e2e99bddab17c11d261194fae0
amatthi <amatthi@emory.edu>
2017-02-17 16:30:26 -0600

    fail

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -80,7 +80,7 @@ class OrderController extends Controller
         $option      = ['description' => $description];
         if (!$this->user->exists) {
             $option['source'] = $post['token'];
-            if ($this->user->voucher->exists) {
+            if ($voucher_code = 'CORE2017') {
         $charge  = $this->user->charge(round($campaign->sale_price * 70), $option);
         $content = [
             'id'            => $charge->id,
@@ -124,7 +124,7 @@ class OrderController extends Controller
     }
     else {
 
-        if ($this->user->voucher->exists) {
+        if ($voucher_code = 'CORE2017') {
     $charge  = $this->user->charge(round($campaign->sale_price * 70), $option);
     $content = [
         'id'            => $charge->id,
