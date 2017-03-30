f48181824f0e201d9dc65a0b18638d00e6939637
amatthi <amatthi@emory.edu>
2017-03-30 18:55:42 -0400

    gulp

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -11418,7 +11418,7 @@ chisel.controller("mainController", function($scope, $rootScope, $upload, mainFa
     $scope.submit_payment = function() {
         mainFactory.pay($scope.__payment).then(function(r) {
             var $form = $('#payment-form');
-            $form.find('.submit').prop('disabled', false);
+            $form.find('.submit').prop('disabled', true);
             $scope.__payment.r = r.data;
             $scope.$broadcast('payment_done');
         }, $scope.handle_error);
